5b784a418f681526b6d2481cf62e7a753dce0788
Inevitabilis <ddd1020018@gmail.com>
2024-10-27 15:41:48 +0300

    moved db init to config

--- src/datasource.ts
+++ src/datasource.ts
@@ -1,17 +1,25 @@
 import { DataSource } from "typeorm";
 import { dbClassesMap } from "./AllDBClasses";
+import config from "../config.json";
 import fs from "fs";
 
-export const AppDataSource = new DataSource({
-  type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: fs.readFileSync("D:\\dbpwd.txt", "utf8").trim(),
-  database: "regionlease",
-  synchronize: true,
-  logging: false,
-  entities: Object.values(dbClassesMap),
-  subscribers: [],
-  migrations: [],
-});
+export function AppDataSource(): DataSource {
+  return (dataSource ??= initDataSource());
+}
+let dataSource: DataSource | undefined = undefined;
+
+function initDataSource(): DataSource {
+  return new DataSource({
+    type: "postgres",
+    host: config.host,
+    port: config.port,
+    username: config.username,
+    password: config.password,
+    database: "regionlease",
+    synchronize: true,
+    logging: false,
+    entities: Object.values(dbClassesMap),
+    subscribers: [],
+    migrations: [],
+  });
+}
--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,19 @@ import { AppDataSource } from "./datasource";
 import { Region } from "./entities/region";
 import { Route } from "./webpageRouting";
 
-async function main() {
-  await AppDataSource.initialize();
+//HOW TO EXTEND DATABASE:
+//make a new entity
+//add it to AllDBClasses.ts
 
-  const regionRepo = AppDataSource.getRepository(Region);
+async function main() {
+  const dataSource = AppDataSource();
+
+  const regionRepo = dataSource.getRepository(Region);
   const regions = await regionRepo.find();
   console.log(JSON.stringify(regions));
 
   const app = express();
-  Route(app, AppDataSource);
+  Route(app, dataSource);
 
   const port = 3000;
   app.listen(port, () => {
@@ -23,7 +27,7 @@ void main();
 
 function shutdown(signal: NodeJS.Signals) {
   console.log(`got shutdown signal (${signal}), shutting down...`);
-  AppDataSource.destroy();
+  AppDataSource().destroy();
   process.exit(0);
 }
 process.on("SIGTERM", shutdown);
